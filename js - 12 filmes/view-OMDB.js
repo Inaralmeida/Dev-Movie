210fa7472bdb0630ee6309199e6cefeb04a7dfc4
inralmeida <inara.ralmeida@gmail.com>
2020-11-26 22:37:18 -0300

    alterei os cards

--- js - 12 filmes/view-OMDB.js
+++ js - 12 filmes/view-OMDB.js
@@ -7,8 +7,8 @@ class viewFilme
         let titulo = document.querySelector('#card-titulo'+i)
         titulo.textContent = dados.getTitulo();
 
-        let descricao = document.querySelector('#card-text'+i)
-        descricao.textContent = dados.getDescricao();
+        /* let descricao = document.querySelector('#card-text'+i)
+        descricao.textContent = dados.getDescricao(); */
 
         let capa = document.querySelector('#card-img-top'+i)
         capa.src = dados.getCapa();
