bf819cc9503c85564f8e125d816d1fc8ff4f22d4
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-11-28 00:05:27 -0200

    Create model.js

---
+++ model.js
@@ -0,0 +1,91 @@
+class UserModelLocalizacao{
+    constructor(cep){
+        this._cep = cep;
+        this._logradouro = "";
+        this._complemento = ""; 
+        this._bairro = ""; 
+        this._localidade = "";  
+        this._uf = ""; 
+        
+    }
+    
+    buscaEndereco(){
+        
+        let cep = this._cep;
+        
+
+       
+        let requisicao  = new XMLHttpRequest(); 
+        requisicao.open("GET",  `https://viacep.com.br/ws/` + cep + `/json/unicode/`  , false)
+        requisicao.addEventListener("load", () => {
+        
+                    let objeto = this._processaResponse(requisicao.responseText); 
+                    this._atualiza (objeto); 
+
+        })
+
+        let cepFinal= cep.split('.').join("").split("-").join(""); //remove o -
+        var validacep = /^[0-9]{8}$/; //regex
+        if(!validacep.test(cepFinal)){
+             document.getElementById("erro").innerHTML = "ERRO!"
+             document.getElementById("cep").value = ""; 
+             document.getElementById("logradouro").value = ""; 
+             document.getElementById("bairro").value = "";  
+             document.getElementById("estado").value = ""; 
+             document.getElementById("UF").value = ""; 
+         
+         }else if(undefined){
+             document.getElementById("logradouro").value = ""; 
+             document.getElementById("bairro").value = "";  
+             document.getElementById("estado").value = ""; 
+             document.getElementById("UF").value = ""; 
+             document.getElementById("erro").innerHTML = ""
+         }else{
+            document.getElementById("logradouro").value = ""; 
+            document.getElementById("bairro").value = "";  
+            document.getElementById("estado").value = ""; 
+            document.getElementById("UF").value = ""; 
+            document.getElementById("erro").innerHTML = ""
+        }
+       
+        requisicao.send();
+    }
+    
+
+    _processaResponse(responseString){
+        let response = JSON.parse(responseString); 
+        return response; 
+    }
+    
+    _atualiza (dados){
+              
+        this._cep = dados.cep;
+        this._logradouro = dados.logradouro; 
+        this._complemento = dados.complemento; 
+        this._bairro = dados.bairro;
+        this._localidade = dados.localidade; 
+        this._uf = dados.uf; 
+
+    
+    }
+  
+    getCep(){
+        return this._cep; 
+    }
+    getLogradouro(){
+        return this._logradouro; 
+    }
+    getComplemento(){
+        return this._complemento; 
+    }
+    getBairro(){
+        return this._bairro; 
+    }
+    getLocalidade(){
+        return this._localidade; 
+    }
+    getUf(){
+        return this._uf; 
+    }
+    
+}
