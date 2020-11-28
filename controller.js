35eb0a598d34c617e5683b3e391ddd6e026e43a2
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-11-28 00:11:48 -0200

    Update controller.js

---
+++ controller.js
@@ -0,0 +1,10 @@
+class UserControllerLocalizacao{
+    buscaEndereco(){
+        let dados = new UserModelLocalizacao(cep.value)
+        dados.buscaEndereco(); 
+        
+        let view = new UserViewLocalizacao();
+        view.render( dados ); 
+    }
+  
+}
